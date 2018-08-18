8d81edc59d70c86cb6b0190cc9b3b85301814288
Jason E. Trout <jsnetrout@gmail.com>
2018-08-18 00:05:48 -0500

    String generation is working

--- syntax/syntax.rb
+++ syntax/syntax.rb
@@ -7,9 +7,9 @@ bnf = [
   '<S>::=<M>',
   '<M>::=generate password with <Q>',
   '<P>::=<N> <T>',
-  '<Q>::=<P>[1,2]',
-  '<N>::=1|2',
-  '<T>::=ucase|numeric'
+  '<Q>::=<P>[1,3]',
+  '<N>::=1|2|3|4|5',
+  '<T>::=ucase|numeric|special'
 ]
 puts 'BNF', bnf
 puts
@@ -56,4 +56,67 @@ simplified_bnf.each {
 
   nonterminals[nonterminal].push(expansion)
 }
-p 'Nonterminals', nonterminals
+puts 'Nonterminals'
+nonterminals.each {
+  |k, v|
+  puts k, "#{v}"
+}
+puts
+
+# Generate strings
+start_time = Time.now
+strings = Set['<S>']
+prev_strings_size = strings.size
+loop do
+  strings_to_add = Set[]
+  puts strings
+  strings.each { 
+    |s|
+    tokens = s.split ' ' 
+    tokens.each_with_index { 
+      |t, i|
+      expansions = nonterminals[t]
+      unless expansions.nil?
+        expansions.each {
+          |e|
+	  working_tokens = s.split ' '
+	  working_str = ''
+	  working_tokens.each_with_index {
+	    |wt, j|
+	    unless j == i
+	     working_str += ' ' +working_tokens[j]
+	    else
+	     working_str += ' ' + e
+	    end
+	  }
+	  working_str.strip
+  	  strings_to_add.add(working_str)
+        }
+      end
+    }
+  }
+  strings.merge(strings_to_add)
+
+  unless strings.size == prev_strings_size
+    prev_strings_size = strings.size
+  else
+    strings_to_remove = []
+    strings.each {
+      |s|
+      if /\<.*?\>/.match(s)
+        strings_to_remove.push(s)
+      end
+    }
+    strings.subtract(strings_to_remove)
+    break
+  end
+end
+stop_time = Time.now
+
+
+strings.each {
+  |s| puts s
+}
+
+print 'Generated ', strings.size, ' strings in ' + (stop_time - start_time).to_s + 's'
+puts
